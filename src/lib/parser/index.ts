3204387472f2c5d61652cf0083d88ae0acf74ae9
peter <2814594602@qq.com>
2025-12-25 11:16:30 +0800

    (feat&fix): fix vercel build error & resume header finish

--- src/lib/parser/index.ts
+++ src/lib/parser/index.ts
@@ -1,3 +1,12 @@
-function matter() {
+function matter(input?: string | { content: string }, options: any) {
+  if (input === '') return {
+    data: {},
+    content: input,
+    excerpt: '',
+    orig: input
+  }
+
   
 }
+
+export default matter;
